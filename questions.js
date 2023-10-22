1674f4f5fd39a2fa54da9f6c97cde9ecf3b10274
HeySkidee <harshitojha46@gmail.com>
2023-10-22 20:11:25 +0530

    initial commit

--- questions.js
+++
@@ -1,8 +0,0 @@
-[
-    {
-        "question": "What does HTML stand for?",
-        "options": ["Hyper Text Markup Language", "Highly Textual Markup Language", "Hyperlink and Text Markup Language"],
-        "answer": "Hyper Text Markup Language"
-    },
-    // Add more questions in a similar format
-]
